8e976fc1924fee8a39a2cfe1c735ae04e9054e92
CrossEyedCat <ArtemZenkevich1@yandex.ru>
2022-09-20 04:01:28 +0300

    version 1.0

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lab_1</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/st.scss">
+</head>
+<body>
+<header>
+    <div class="head_title">Зенкевич Артем Андреевич P3212 Вариант:1210</div>
+</header>
+<?php
+            ini_set('display_errors', 0);
+            error_reporting(0);
+            session_start();
+
+			if (!isset($_SESSION['counter'])) {
+                $_SESSION['counter']=0;}
+            if($_SESSION['counter']<10){
+            		$_SESSION['counter']++;
+            }
+            else{
+                session_unset();
+            }
+                    ?>
+<div class="middle">
+    <div class="target"><img class="target" src="/resources/images/target.png" alt="target"></div>
+    <div class="menu">
+        <h2 class="center__text glitch is-glitching" data-text="Стрельбище">Стрельбище</h2>
+        <h3>Настройка выстрела</h3>
+        <form action ="index.php" method = "get">
+            <div class="R_text">
+                <h4>Значение R:</h4>
+
+                <select class="R" id = "coord" name = "paramR">
+                    <option  selected value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+                </div>
+            <div class="X_text" id = "coord">
+                <h4>Значение X:</h4>
+                    <textarea class = "number" name="paramX" id="num_X"></textarea>
+            </div>
+            <div class="Y_text" id = "coord">
+                <h4>Значение Y:</h4>
+                    <textarea class = "number" name="paramY" id="num_Y"></textarea>
+            </div>
+               <input type="submit" class="button">
+        </form>
+    </div>
+</div>
+<?php
+			include 'script.php'; //определение попадания точки в область происходит здесь. ВАЖНО! в файле .html эта строка не будет работать
+?>
+<script>
+    //здесь мы не даём пользователю ввести в поле для Y ввести не те данные
+    function onlyDigits() {
+        this.value = this.value.replace(/[^\d\,\-]/g, "");//разрешаем ввод только цифр 0-9, запятой и минуса
+
+        if(this.value.lastIndexOf("-")> 0) {//если пользователь вводит тире (минус) не самым первым символом...
+            this.value = this.value.replace("-", "");//то удаляем этот минус
+        }
+
+        if(this.value[0]== "-") {//если первый символ это минус (число отрицательно)...
+            if(this.value[1]== "6" || this.value[1]== "7" || this.value[1]== "8" || this.value[1]== "9"){
+                this.value = this.value.replace(this.value[1], "");//то запрещаем вводить цифры 4,5,6,7,8,9
+            }
+            if(this.value[2]!=","){
+                this.value = this.value.replace(",", "");//то запрещаем вводить число больше 0
+            }
+
+            if(this.value.length>2 && this.value[2]!=",") this.value=this.value[0]+this.value[1]+","+this.value[2];//если третий символ введён и он не запятая, то вставляем запятую между вторым и третим символом
+            if(this.value.length>8) this.value = this.value[0]+this.value[1]+this.value[2]+this.value[3]+this.value[4]+this.value[5]+this.value[6]+this.value[7];//если количество символов равно 8 (5 знаков после запятой), не даём вводить больше
+        }else{//если число положительно (первым введён не минус, а цифра)...
+            if(this.value[0]== "," || this.value[0]== "6" || this.value[0]== "7" || this.value[0]== "8" || this.value[0]== "9"){
+                this.value = "";//то эта цифра должна быть от 0 до 5
+            }
+            if(this.value[0]== "5" && this.value[1]!= ""){
+                this.value = this.value[0];//то эта цифра должна быть от 0 до 5
+            }
+
+            if(this.value.length>1 && this.value[1]!=",") this.value=this.value[0]+","+this.value[1];//если второй символ введён и он не запятая, то вставляем запятую между первым и вторым символом
+            if(this.value.length>7) this.value = this.value[0]+this.value[1]+this.value[2]+this.value[3]+this.value[4]+this.value[5]+this.value[6];//если количество символов равно 7 (5 знаков после запятой), не даём вводить больше
+        }
+
+        if(this.value.match(/\,/g).length > 1) {//не даём ввести больше одной запятой
+            this.value = "";
+        }
+
+        if(this.value.match(/\-/g).length > 1) {//не даём ввести больше одного минуса
+            this.value = "";
+        }
+    }
+    //присваиваем данный скрипт нашему полю ввода
+    document.querySelector('#num_X').onkeyup = onlyDigits
+    document.querySelector('#num_Y').onkeyup = onlyDigits
+
+    //следующие строчки я скопипастил со stackoverflow и они не дают выбирать больше одного checkbox-а. Работает - не трогай
+</script>
+<footer>
+    <div class="foot_title">Зенкевич Артем Андреевич</div>
+</footer>
+</body>
+</html>
--- script.php
+++ script.php
@@ -1,86 +1,90 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Лабораторная работа №1 //Зенкевич Артем</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<link rel="stylesheet" type="text/css" href="css/style2.css">
-    </head>
-    <body>
-        <?php
+<head>
+    <title>Лабораторная работа №1 //Зенкевич Артем</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+<body>
+<?php
+        ini_set('display_errors', 0);
+        error_reporting(0);
         //запоминаем время начала работы скрипта
         $start = microtime(true);
         //получаем дату и время по москве
         date_default_timezone_set('Europe/Moscow');
         $now= date("d.m.y H:i");
-        //получаем параметры из Shooting gallery.php
-        $coordX=$_POST['paramX'];
-        $coordYSee=$_POST['paramY'];
-        $paramR=$_POST['paramR'];
-        //меняем в Y запятую на точку
-        $coordNY=explode(",",$coordYSee);
-        $coordY=$coordNY[0].".".$coordNY[1];
-	//готовимся к вычислению попадания в область
-        $halfR=$paramR/2;
-        $chetvert=0;
-	//узнаём четверть, в которой располагается точка
-        if($coordX>0 && $coordY>0) $chetvert=2;//правая верхняя
-        if($coordX<0 && $coordY>0) $chetvert=1;//левая верхняя
-        if($coordX>0 && $coordY<0) $chetvert=3;//правая нижняя
-        if($coordX<0 && $coordY<0) $chetvert=4;//левая нижняя
-        if ($chetvert==1){//в этой четверти совсем нет области, так что всё просто
-           $message= "Точка не входит в область";
-        }
+        //получаем параметры из index.php
 
-        if ($chetvert==2){//вспоминаем уравнение наклонной прямой y=kx+b, где b=-1, и если ниже, то входит, если выше, то нет
-            $yfunk=-$coordX+$halfR;
-            if($yfunk>=$coordY){
-                $message= "Точка входит в область";
-            }else{
-                $message= "Точка не входит в область";
+        $coordXSee=$_GET['paramX'];
+        $coordYSee=$_GET['paramY'];
+        if($coordXSee=="" || $coordYSee==""){
+            session_unset();
+            if($coordXSee=="" && $coordYSee==""){
+                $message="Вы не ввели значение X и Y";
+            }else if($coordXSee==""){
+                $message="Вы не ввели значение X";
             }
-        }
-
-        if ($chetvert==3){//поскольку прямоугольник, то X<=R и Y<=R/2. Если да, то в области. нет - нет
-            if($coordY<=$halfR && $coordX<=$paramR){
-                $message= "Точка входит в область";
-            }else{
-                $message= "Точка не входит в область";
+            else if($coordYSee==""){
+                $message="Вы не ввели значение Y";
             }
-        }
-
-        if ($chetvert==4){//окружность с центром в точке (0;0). Находим расстояние от нашей точки до центра окружности, и если оно больше R/2, то точка не в области.
-            $distance=$coordX*$coordX+$coordY*$coordY;
-            $distance=sqrt($distance);
-            if ($distance>$halfR){
-                $message= "Точка не входит в область";
-            }else{
-                $message= "Точка входит в область";
-            }
-        }
-        if ($chetvert==0){//если точка лежит на координатной оси (X=0 или Y=0)
-            if($coordX==0){//если лежит на оси y...
-                if($coordY<=$halfR && $coordY>=-$halfR) {//то координата Y должна быть в диапазоне [R/2; -R/2]
-                    $message="Точка входит в область";
-                }else{
-                    $message="Точка не входит в область";
-                }
-            }else{//если лежит на оси x...
-                if($coordX<=$paramR && $coordX>=-$halfR) {//то координата X должна быть в диапазоне [R; -R/2]
-                    $message="Точка входит в область";
-                }else{
-                    $message="Точка не входит в область";
+        }else {
+            $paramR = $_GET['paramR'];
+            //меняем в Y и X запятую на точку
+            $coordNY = explode(",", $coordYSee);
+            $coordNX = explode(",", $coordXSee);
+            $coordY = $coordNY[0] . "." . $coordNY[1];
+            $coordX = $coordNX[0] . "." . $coordNX[1];
+            //готовимся к вычислению попадания в область
+            $halfR = $paramR / 2;
+            $chetvert = 0;
+            //узнаём четверть, в которой располагается точка
+            if ($coordX >= 0 && $coordY >= 0) $chetvert = 2;//правая верхняя
+            if ($coordX <= 0 && $coordY >= 0) $chetvert = 1;//левая верхняя
+            if ($coordX >= 0 && $coordY <= 0) $chetvert = 3;//правая нижняя
+            if ($coordX <= 0 && $coordY <= 0) $chetvert = 4;//левая нижняя
+            if ($chetvert == 1) {//в этой четверти совсем нет области, так что всё просто
+                if ($coordX >= -$halfR && $coordY <= $paramR) {
+                    $message = "Точка входит в область";
+                } else {
+                    $message = "Точка не входит в область";
                 }
             }
-        }
-	//получаем время окончания работы скрипта
-        $finish = microtime(true);
-	//высчитываем время работы (разницу) и округляем
-        $timeWork=$finish-$start;
-        $timeWork=round($timeWork,7);
-	//заполняем переменную сессии для отображения всей таблицы
-        $ses=$_SESSION['counter'];
-        $ses++;
-        $_SESSION['str_'.$ses]="    <tr>
+
+            if ($chetvert == 2) {//вспоминаем уравнение наклонной прямой y=kx+b, где b=-1, и если ниже, то входит, если выше, то нет
+                if (($coordX <= $halfR && $coordY == 0) || ($coordY <= $paramR && $coordX == 0)) {
+                    $message = "Точка входит в область";
+                } else {
+                    $message = "Точка не входит в область";
+                }
+            }
+
+            if ($chetvert == 3) {//поскольку прямоугольник, то X<=R и Y<=R/2. Если да, то в области. нет - нет
+                if ($coordX + $coordY >= -$paramR) {
+                    $message = "Точка входит в область";
+                } else {
+                    $message = "Точка не входит в область";
+                }
+            }
+
+            if ($chetvert == 4) {//окружность с центром в точке (0;0). Находим расстояние от нашей точки до центра окружности, и если оно больше R/2, то точка не в области.
+                $distance = $coordX * $coordX + $coordY * $coordY;
+                $distance = sqrt($distance);
+                if ($distance > $halfR) {
+                    $message = "Точка не входит в область";
+                } else {
+                    $message = "Точка входит в область";
+                }
+            }
+            //получаем время окончания работы скрипта
+            $finish = microtime(true);
+            //высчитываем время работы (разницу) и округляем
+            $timeWork = $finish - $start;
+            $timeWork = round($timeWork, 7);
+            //заполняем переменную сессии для отображения всей таблицы
+            $ses = $_SESSION['counter'];
+            $ses++;
+            $_SESSION['str_' . $ses] = "    <tr>
         <td> $now</td>
         <td> $timeWork с</td>
         <td>$coordX </td>
@@ -88,12 +92,12 @@
         <td> $paramR </td>
         <td> $message </td>
     </tr>";
-
+        }
 ?>
 <!--Выводим сообщение о результате-->
-<h3><center><?php echo $message; ?></center></h3>
+<link rel="stylesheet" href="css/style2.css">
+<h3 class="message_return"><?php echo $message; ?></h3>
 <br>
-<center>
 <table>
     <tr>
         <td>Дата и время запроса</td>
@@ -108,11 +112,8 @@
     for($m=1;$m<=$ses;$m++){
         echo $_SESSION['str_'.$m];
     }
-    ?>
 
+    ?>
 </table>
-</center>
-    </body>
+</body>
 </html>
-Footer
-© 2022 GitHub, Inc.
